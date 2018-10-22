9f89419afe2daf9162282d91c6ecede983c47e13
Javier G. Sogo <jgsogo@gmail.com>
2018-10-22 11:06:03 +0200

    fill fields

--- conanfile.py
+++ conanfile.py
@@ -4,48 +4,51 @@ from conans import tools
 
 class TgbotConan(ConanFile):
     name = "tgbot"
-    version = "0.0"
-    license = "<Put the package license here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Tgbot here>"
+    version = "b35438d"
+
+    home = "http://reo7sp.github.io/tgbot-cpp"
+    license = "MIT License"
+    description = "C++ library for Telegram bot API"
+
+    url = "https://github.com/jgsogo/conan-tgbot"
+
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
+    default_options = {"shared": False}
     generators = "cmake"
+
+    @property
+    def _source_subfolder(self):
+        return "tgbot-cpp"
 
     def requirements(self):
         self.requires("OpenSSL/1.1.1@conan/stable")
         self.requires("boost/1.68.0@conan/stable")
+        self.requires("libcurl/7.61.1@bincrafters/stable")
 
     def source(self):
-        self.run("git clone https://github.com/reo7sp/tgbot-cpp.git")
-        self.run("cd tgbot-cpp && git checkout master")
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("tgbot-cpp/CMakeLists.txt", "project(TgBot)",
+        self.run("git clone https://github.com/reo7sp/tgbot-cpp.git {}".format(self._source_subfolder))
+        self.run("cd {} && git checkout {}".format(self._source_subfolder, self.version))
+
+        tools.replace_in_file("{}/CMakeLists.txt".format(self._source_subfolder),
+                              "project(TgBot)",
                               '''project(TgBot)
-set(CMAKE_CXX_STANDARD 11)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
 
-    def build(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="tgbot-cpp")
+        cmake.definitions['BUILD_SHARED_LIBS'] = "ON" if bool(self.options.shared) else "OFF"
+        cmake.configure(source_folder=self._source_subfolder)
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        self.copy("*.h", dst="include", src="tgbot-cpp/include")
-        self.copy("*TgBot.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["TgBot"]
